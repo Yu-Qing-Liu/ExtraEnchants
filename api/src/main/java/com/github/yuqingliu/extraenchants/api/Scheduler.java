61c3cc430a701bec685e61351d6a459e2de83d9e
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2024-05-26 18:52:14 -0400

    scheduler

--- api/src/main/java/com/github/yuqingliu/extraenchants/api/Scheduler.java
+++ api/src/main/java/com/github/yuqingliu/extraenchants/api/Scheduler.java
@@ -1,6 +1,8 @@
 package com.github.yuqingliu.extraenchants.api;
 
+import lombok.AccessLevel;
 import lombok.Setter;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 
@@ -13,37 +15,33 @@ import java.util.function.Consumer;
 public final class Scheduler {
     private Scheduler() {}
 
-    @Setter
+    @Setter(AccessLevel.PACKAGE)
     private static JavaPlugin plugin;
 
-    public static BukkitTask runLater(Consumer<Task> consumer, Duration duration) {
-        Task task = new Task(plugin, consumer);
-        task.runTaskLater(duration);
-        return task.getTask();
+    // 1s == 1000 ms == 20 ticks
+    //    -> 1 tick == 50 ms
+    private final static double MS_PER_TICKS = 50;
+    public static long toTicks(Duration duration) {
+        return (long) (duration.toMillis() / MS_PER_TICKS);
     }
 
-    public static BukkitTask runTimer(Consumer<Task> consumer, Duration period, Duration wait) {
-        Task task = new Task(plugin, consumer);
-        task.runTaskTimer(wait, period);
-        return task.getTask();
+    public static BukkitTask runAsync(Consumer<BukkitTask> runnable) {
+        return Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> runnable.accept(null));
     }
 
-    public static BukkitTask runAsync(Consumer<Task> consumer) {
-        Task task = new Task(plugin, consumer);
-        task.runAsyncTask();
-        return task.getTask();
+    public static BukkitTask runLater(Consumer<BukkitTask> runnable, Duration duration) {
+        return Bukkit.getScheduler().runTaskLater(plugin, () -> runnable.accept(null), toTicks(duration));
     }
 
-    public static BukkitTask runLaterAsync(Consumer<Task> consumer, Duration duration) {
-        Task task = new Task(plugin, consumer);
-        task.runAsyncTaskLater(duration);
-        return task.getTask();
+    public static BukkitTask runLaterAsync(Consumer<BukkitTask> runnable, Duration duration) {
+        return Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, () -> runnable.accept(null), toTicks(duration));
     }
 
-    public static BukkitTask runTimerAsync(Consumer<Task> consumer, Duration period, Duration wait) {
-        Task task = new Task(plugin, consumer);
-        task.runAsyncTaskTimer(wait, period);
-        return task.getTask();
+    public static BukkitTask runTimer(Consumer<BukkitTask> runnable, Duration period, Duration wait) {
+        return Bukkit.getScheduler().runTaskTimer(plugin, () -> runnable.accept(null), toTicks(wait), toTicks(period));
+    }
+
+    public static BukkitTask runTimerAsync(Consumer<BukkitTask> runnable, Duration period, Duration wait) {
+        return Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, () -> runnable.accept(null), toTicks(wait), toTicks(period));
     }
 }
-
