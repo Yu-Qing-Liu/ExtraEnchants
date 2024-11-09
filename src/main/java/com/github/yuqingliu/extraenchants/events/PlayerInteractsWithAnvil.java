8be367baefe225b74c6a8c2e6f1169de150e3ab0
Yu Qing Liu <yu.qing.liu@mail.mcgill.ca>
2024-11-09 14:25:05 -0500

    updates

--- src/main/java/com/github/yuqingliu/extraenchants/events/PlayerInteractsWithAnvil.java
+++ src/main/java/com/github/yuqingliu/extraenchants/events/PlayerInteractsWithAnvil.java
@@ -1,15 +1,10 @@
 package com.github.yuqingliu.extraenchants.events;
 
-import java.util.Arrays;
-
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.block.Block;
 
 import com.github.yuqingliu.extraenchants.api.managers.InventoryManager;
@@ -17,8 +12,6 @@ import com.github.yuqingliu.extraenchants.view.anvilmenu.AnvilMenu;
 import com.google.inject.Inject;
 
 import lombok.RequiredArgsConstructor;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
 
 import org.bukkit.event.block.Action;
 
@@ -34,12 +27,6 @@ public class PlayerInteractsWithAnvil implements Listener {
         
         if (event.getAction() == Action.RIGHT_CLICK_BLOCK && block != null && block.getType() == Material.ANVIL) {
             event.setCancelled(true);
-            ItemStack item = new ItemStack(Material.NETHERITE_SWORD);
-            ItemMeta meta = item.getItemMeta();
-            meta.lore(Arrays.asList(Component.text("Some Lore", NamedTextColor.WHITE)));
-            item.setItemMeta(meta);
-            Inventory playerInventory = player.getInventory();
-            playerInventory.addItem(item);
             inventoryManager.getInventory(AnvilMenu.class.getSimpleName()).open(player);
         }
     }
